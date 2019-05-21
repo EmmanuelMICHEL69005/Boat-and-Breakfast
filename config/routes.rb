8d3280b6389dd748fca1f7903cc60334dcea3001
MICHEL <michelemmanuel@hotmail.com>
2019-05-21 12:13:52 +0200

    routes et controller boats

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  get 'boats/index'
+  get 'boats/new'
+  get 'boats/show'
+  get 'boats/create'
+  get 'boats/upade'
   devise_for :users
   root to: 'pages#home'
+  resources :boats
+  resources :users
+  resources :bookings
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
