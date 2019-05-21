17311782f2069c637ab85bea7d30e71f407d7146
Raphael Maman <raphaelmamans@gmail.com>
2019-05-21 15:25:54 +0200

    modif forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   skip_before_action :authenticate_user!, only: [:home]
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :phone_number])
+  end
 end
